c03ea3f49bda744c7b5794a71d2c41978030157e
junzvb <junaidvbergfarm@gmail.com>
2026-02-20 02:05:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,14 @@
+require('dotenv').config();
+const TelegramBot = require('node-telegram-bot-api');
 
+const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: false });
+
+async function sendAlert(message) {
+    try {
+        await bot.sendMessage(process.env.CHAT_ID, message);
+    } catch (err) {
+        console.error('Telegram send error:', err);
+    }
+}
+
+module.exports = { sendAlert };
