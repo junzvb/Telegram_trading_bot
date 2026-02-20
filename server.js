aec6c8f9805bba67537d83816805c98fcb6ee1d3
junzvb <junaidvbergfarm@gmail.com>
2026-02-20 02:07:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
+app.get('/', (req, res) => res.send('Telegram Trading Alert Bot Running'));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
